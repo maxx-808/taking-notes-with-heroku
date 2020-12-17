3b3466382a261532d3f03a23b0d29b75c6cf64a6
Maxx <maxhiga.hawaii@gmail.com>
2020-12-16 17:28:11 -1000

    added post to api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -10,4 +10,22 @@ router.get("/notes", (req, res) => {
   });
 });
 
+router.post("/notes", (req, res) => {
+  const newNote = req.body;
+  const newId = uniqid();
+
+  newNote.id = newId;
+
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    let allsaved = JSON.parse(data);
+
+    fs.writeFile("./db/db.json", JSON.stringify(allsaved), (err) => {
+      if (err) return res.JSON({ err: "problem adding" });
+      console.log("Note created!");
+    });
+  });
+  res.redirect("/notes");
+});
+
 module.exports = router;
