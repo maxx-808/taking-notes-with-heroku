29a2783f1b53a47b05cf0d13e9091ff99fd70264
Maxx <maxhiga.hawaii@gmail.com>
2020-12-16 17:18:13 -1000

    added routes

---
+++ routes/api-routes.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const fs = require("fs");
+const path = require("path");
+const uniqid = require("uniqid");
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+const apiRoutes = require("./routes/api-routes");
+const clientRoutes = require("./routes/client-routes");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
