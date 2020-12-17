0f548a17a27d116b323f65009ba403d2db7135f4
Maxx <maxhiga.hawaii@gmail.com>
2020-12-16 17:20:18 -1000

    added to routes. started routes code

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,4 +3,11 @@ const fs = require("fs");
 const path = require("path");
 const uniqid = require("uniqid");
 
+router.get("/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -8,4 +8,8 @@ const clientRoutes = require("./routes/client-routes");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use("/api", apiRoutes);
+app.use("/", clientRoutes);
+app.use(express.static(__dirname + "/public"));
+
 app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
